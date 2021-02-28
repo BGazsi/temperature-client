b067f22dbece6100a49ea6f7d5a9bfb19edb9771
BGazsi <bgazsi94@gmail.com>
2021-02-28 18:37:10 +0100

    sse, mobile menu

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import reducer from './store/reducers/rootReducer';
 const store = createStore(
   reducer,
   // eslint-disable-next-line no-underscore-dangle
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
 ReactDOM.render(
